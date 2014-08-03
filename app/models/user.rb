011fd820f351244af6959dc565623cbe673b45d4
Kartik Singal <ka4tik@gmail.com>
2014-08-03 15:52:57 +0530

    added validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ActiveRecord::Base
+  email_regex= /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+
+  validates :name, :presence => true,
+            :length => {:maximum =>50,:minimum =>3}
+  validates :email, :presence => true,
+            :format => {:with => email_regex},
+            :uniqueness => {:case_sensitive => false}
+
 end
