e077ad63aed77a1199c7e75448dc146e07d054b8
Kartik Singal <ka4tik@gmail.com>
2014-08-04 02:57:04 +0530

    editing and updating users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,21 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+
+  def edit
+    @user=User.find(params[:id])
+  end
+
+  def update
+    @user=User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success]="Profile Updated"
+      redirect_to user_path(@user)
+    else
+      render 'edit'
+    end
+  end
+
   def user_params
     params.require(:user).permit(:name, :email, :password)
   end
