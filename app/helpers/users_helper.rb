1c341fc856ce212ce8d091729252f5c7673571ba
Kartik Singal <ka4tik@gmail.com>
2014-08-04 00:33:42 +0530

    gravator image tag fixed

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,5 @@
 module UsersHelper
   def gravator_for(user,options={:size =>50})
-    raw("<img height='50px'>")
-    #todo: install gravatar gem and uncomment line below
-    # gravatar_image_tag(user.email.downcase,:alt=>user.name,:class=>'gravator',:gravator=>options)
+    gravatar_image_tag(user.email.downcase,:alt=>user.name,:class=>'gravator',:gravator=>options)
   end
 end
