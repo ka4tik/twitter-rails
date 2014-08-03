d479c6d5209bba6181d905fc5de5a3cdc0693874
Kartik Singal <ka4tik@ka4tik-Latitude-E5420.(none)>
2014-08-03 13:55:50 +0530

    added basic layout structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Twitter::Application.config.secret_key_base = '9d4e4116a43ff6ce751d89309417a7541ca8c9291e4ce3ea6418c6795c2e573bd5be10dfc37e7381e4a99569994cef585699114326279fd0f1e133687613c3b9'
