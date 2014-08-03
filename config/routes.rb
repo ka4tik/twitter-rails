207574ae949ee6bc57eb0a31bc0722aea6ffdfd4
Kartik Singal <ka4tik@ka4tik-Latitude-E5420.(none)>
2014-08-03 14:49:18 +0530

    made links work

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Twitter::Application.routes.draw do
+  get '/contact', :to => 'pages#contact'
+  get '/about', :to=> 'pages#about'
   get "pages/home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
